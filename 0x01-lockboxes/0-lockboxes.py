cf67fc5f61b7cd3fb7325bbea6fa810bbd95a3d1
thorbie010 <jonahemmanuel54@gmail.com>
2024-01-11 23:17:19 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,23 +2,15 @@
 """ opening lockboxes """
 
 def canUnlockAll(boxes):
-    if not boxes or not boxes[0]:
-        return False
+    """ Function that checks if all boxes can be opened."""
+    my_boxes = set()
 
-    # Initialize a set to keep track of opened boxes
-    opened_boxes = {0}
+    def dfs(current_box):
+        my_boxes.add(current_box)
 
-    # Initialize a stack to perform depth-first search
-    stack = [0]
-
-    while stack:
-        current_box = stack.pop()
-
-        # Check keys in the current box
         for key in boxes[current_box]:
-            if key not in opened_boxes and key < len(boxes):
-                opened_boxes.add(key)
-                stack.append(key)
+            if key not in my_boxes:
+                dfs(key)
+    dfs(0)
 
-    # Check if all boxes can be opened
-    return len(opened_boxes) == len(boxes)
+    return len(my_boxes) == len(boxes)
