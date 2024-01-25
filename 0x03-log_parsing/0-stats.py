b937c9d03d04bb98d6d57dc6046feb911b9b69b1
Jonah <JonahEmmanuel54@gmail.com>
2024-01-25 13:55:17 +0100

    fixing issues

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,19 @@
 #!/usr/bin/python3
-""" Script that reads stdin line by line and computes metrics. """
-
-from sys import stdin
-
-
-status_codes = {
-        '200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0
-        }
-size = 0
+"""Comment"""
+import sys
 
 
-def compute_metric():
-    """ Function that reads and computes stdin. """
-
-    print("File size: {}".format(size))
-    for key, val in sorted(status_codes.items()):
-        if val > 0:
-            print("{}: {}".format(key, val))
-
-
-if __name__ == '__main__':
-    try:
-        for i, line in enumerate(stdin, 1):
-            try:
-                my_data = line.split()
-                size += int(my_data[-1])
-                if my_data[-2] in status_codes.keys():
-                    status_codes[my_data[-2]] += 1
-            except Exception:
-                pass
-            if not i % 10:
-                compute_metric()
-
-    except KeyboardInterrupt:
-        compute_metric()
-        raise
-    compute_metric()
+if __name__ == "__main__":
+    size = 0
+    code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    for index, line in enumerate(sys.stdin, start=1):
+        line_size = len(line.encode("utf-8"))
+        size += line_size
+        items = line.strip().split()
+        key = int(items[7])
+        code[key] += 1
+        if index % 10 == 0:
+            print("File size: {}".format(size))
+            for k, v in code.items():
+                if v > 0:
+                    print("{}: {}".format(k, v))
