000769a54f3ccf535669e11bfd8224ba66bdf79c
jonah <jonahemmanuel54@gmail.com>
2024-02-22 12:49:07 +0100

    rotate 2d matrix

--- 0x00-pascal_triangle/0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x00-pascal_triangle/0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-rotate 2d matrix
-"""
+"""Rotate 2d matrix"""
+
 
 def rotate_2d_matrix(matrix):
     """rotating 2d matrix"""
-    n = len(matrix)
 
+    n = len(matrix)
     for i in range (n):
         for j in range (i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-            for i in range(n):
-                matrix[i].reverse()
+    for i in range(n):
+        matrix[i].reverse()
