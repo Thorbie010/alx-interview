8866b4a2ff0655471225d4cc514145f243e1cb9f
jonah <jonahemmanuel54@gmail.com>
2024-03-01 00:27:04 +0100

    utf 8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+script to determines if a given data set 
+represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """method that determines if a given data set represents 
+    a valid UTF-8 encoding.
+    """
+    num_bytes_to_follow = 0
+    
+    for byte in data:
+        byte = byte & 0xFF  # Ensure only 8 least significant bits are considered
+        
+        if num_bytes_to_follow == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                num_bytes_to_follow = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes_to_follow = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes_to_follow = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes_to_follow -= 1
+    
+    return num_bytes_to_follow == 0
